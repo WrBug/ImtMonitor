2658ad71a0abc89e0084b5ee0067c08878b6a3f3
WrBug <i@wrbug.com>
2018-03-16 16:19:16 +0800

    version1.1.0

--- account.py
+++ account.py
@@ -1,3 +1,30 @@
 # coding:utf-8
 
 class Account:
+    # 云搬家账号
+    IMT_USER = 'IMT_USER_INPUT'
+
+    # 云搬家密码
+    IMT_PASS = 'IMT_PASS_INPUT'
+
+    #发送消息方式 1. 邮件  2. 微信
+    SEND_TYPE='SEND_TYPE_INPUT'
+
+    #ftqq配置
+
+    FTQQ_SCKEY='FTQQ_SCKEY_INPUT'
+
+
+    # 邮件配置
+
+    # 接受邮件的邮箱，替换成自己的邮箱
+    EMAIL = 'EMAIL_INPUT'
+
+    # 邮件stmp配置
+    # 邮箱账号
+    FROM_EMAIL = 'FROM_EMAIL__INPUT'
+    # 邮箱密码，部分邮箱需要申请授权码，如qq邮箱
+    EMAIL_PASSWORD = 'EMAIL_PASSWORD_INPUT'
+
+    # smtp服务器
+    SMTP_SERVER = 'SMTP_SERVER_INPUT'
